a100ade7f6df0cc8856c63fa81687f304115f747
ViniciusAz <viniciussazevedo@gmail.com>
2018-08-27 12:24:21 -0300

    Lendo arquivo de texto para gerar Frequencia

--- App.java
+++ App.java
@@ -1,24 +1,26 @@
 import java.util.Hashtable;
+import java.io.*;
 
 public class App {
-	
-	public static void main(String[] args) {
-		
-		//FreqTable table = new FreqTable(3);
-		
-		Hashtable<Character,Double> table = new Hashtable<Character,Double>();
-		
-		table.put('a', 45.0);
-		table.put('b', 13.0);
-		table.put('c', 12.0);
-		table.put('d', 16.0);
-		table.put('e', 9.0);
-		table.put('f', 5.0);
-		
-		HuffmanEncoding hf = new HuffmanEncoding(table);
-		
+
+	public static void main(String[] args) throws Exception {
+
+		FileReader fr = new FileReader(new File(args[0]));
+		String texto = "";
+		int i = fr.read();
+		while(i != -1)
+		{
+			texto += (char)i;
+			i = fr.read();
+		}
+		fr.close();
+
+		FreqTable table = new FreqTable(texto);
+
+		HuffmanEncoding hf = new HuffmanEncoding(table.envia());
+
 		hf.printEncoding();
-		
+
 	}
-	
+
 }
--- FreqTable.java
+++ FreqTable.java
@@ -1,10 +1,26 @@
+//Objetivo ? recebe um texto qualquer, gera uma
+//tabela de frequencia de ocorrencias de caracteres
+
+import java.util.Hashtable;
+
 public class FreqTable {
 
+	private Hashtable<Character,Double> table;
 
+	public FreqTable (String texto) {
+    table = new Hashtable<Character,Double>();
 
-  //FreqTable table = new FreqTable(3);
-	public FreqTable ( /*int ?*/ ) {
-    //TODO
+		for (int i = 0; i<texto.length(); i++) {
+			put(texto.charAt(i));
+		}
   }
 
+	public void put(Character s) {
+		if(!table.containsKey(s)) table.put(s,(double) 0);
+		else table.put(s,table.get(s)+1);
+	}
+
+	public Hashtable<Character,Double> envia(){
+		return table;
+	}
 }
